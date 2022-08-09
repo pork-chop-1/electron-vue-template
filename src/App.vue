6fd80ba39c4d17520f7f2f47fe01ef6f34dcf944
pork-chop-1 <2470017654@qq.com>
2022-08-09 16:28:11 +0800

    1

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>💖 Hello World!</h1>
+    <p>Welcome to your Electron application.</p>
+  </div>
+</template>
+<script lang="ts" setup>
+  console.log('ts enable')
+</script>
+<style>
+  
+</style>
