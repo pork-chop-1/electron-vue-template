2cae42073d986589b5344d345cb4e2be736563fa
Tom <2470017654@qq.com>
2022-08-26 16:43:21 +0800

    12

---
+++ src/hooks/useClickMenu/index.ts
@@ -0,0 +1,9 @@
+
+import BMenu from '@/components/BMenu/index.vue'
+
+type MenuItemType = {}
+function useClickMenu(container: HTMLElement, menuInfo: MenuItemType[]) {
+
+}
+
+export default useClickMenu
