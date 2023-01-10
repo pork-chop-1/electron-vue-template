ccc46c765d22c450ebc24c376c7cb3e9f50f774a
Tom <2470017654@qq.com>
2023-01-10 16:55:45 +0800

    ~

--- src/components/BPagination/index.vue
+++ src/components/BPagination/index.vue
@@ -1,23 +1,41 @@
 <template>
   <div class="pagination-container">
-    <button :disabled="current === 1" @click="currentChange(current - 1)">&lt;</button>
-    <button :class="{current: current === 1}" @click="currentChange(1)">1</button>
+    <button
+      :disabled="current === 1"
+      @click="currentChange(current - 1)"
+      class="pagination-item"
+    >&lt;</button>
+    <button
+      :class="{current: current === 1}"
+      @click="currentChange(1)"
+      class="pagination-item"
+    >1</button>
     <!-- 中间列表长度不为0并且第一个不是2 -->
-    <div class="left-dots" v-show="centerList.length !== 0 && centerList[0] != 2">...</div>
+    <div
+      class="left-dots pagination-ellipsis"
+      v-show="centerList.length !== 0 && centerList[0] != 2"
+    >•••</div>
     <button
       v-for="i in centerList"
       :key="i"
       :class="{ current: current === i}"
+      class="pagination-item"
       @click="currentChange(i)">{{ i }}</button>
     <!-- 中间列表长度不为0并且最后一个不是pageSize-1 -->
     <div
-      class="right-dots"
-      v-show="centerList.length !== 0 && centerList[centerList.length - 1] != pageSize - 1">...</div>
+      class="right-dots pagination-ellipsis"
+      v-show="centerList.length !== 0 && centerList[centerList.length - 1] != pageSize - 1"
+    >•••</div>
     <button
       :class="{ current: current === pageSize}"
       v-show="pageSize > 1"
+      class="pagination-item"
       @click="currentChange(pageSize)">{{ pageSize }}</button>
-    <button :disabled="current === pageSize"  @click="currentChange(current + 1)">&gt;</button>
+    <button
+      :disabled="current === pageSize"
+      class="pagination-item"
+      @click="currentChange(current + 1)"
+    >&gt;</button>
   </div>
 </template>
 <script lang="ts" setup>
@@ -57,7 +75,9 @@ watch(current, v => {
 }, {immediate: true})
 
 const currentChange = (now: number) => {
-  emits('update:current', now)
+  if(now !== current.value) {
+    emits('update:current', now)
+  }
 }
 </script>
 <style lang="scss">
@@ -66,8 +86,29 @@ const currentChange = (now: number) => {
   align-items: center;
   justify-content: center;
 
+  .pagination-item {
+    min-width: 32px;
+    height: 32px;
+    border: 1px solid #d9d9d9;
+    background-color: var(--plain-bg);
+    color: var(--plain-fg);
+    border-radius: 2px;
+    margin-right: 8px;
+    cursor: pointer;
+
+    &:disabled {
+      background-color: var(--plain-bg-hover);
+      cursor: not-allowed;
+    }
+  }
+
   .current {
-    background-color: var(--theme-bg)
+    border-color: var(--theme-bg);
+    color: var(--theme-bg);
+  }
+
+  .pagination-ellipsis {
+    margin-right: 8px;
   }
 }
 </style>
